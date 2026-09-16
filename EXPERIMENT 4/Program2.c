a12497e33dc6b17dad050c5db0accbec08140ede
nisargptl0410-ops <nisargptl04.10@gmail.com>
2026-09-16 22:28:55 +0530

    Add files via upload

---
+++ EXPERIMENT 4/Program2.c
@@ -0,0 +1,23 @@
+//Experiment-4
+//Program-2
+
+#include <stdio.h>
+int main()
+{
+    int n, i, fact = 1;
+
+    //Takes number from the user
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    //Calculates the Factorial
+    for(i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+
+    //Displays the Factorial
+    printf("Factorial = %d", fact);
+
+    return 0;
+}
