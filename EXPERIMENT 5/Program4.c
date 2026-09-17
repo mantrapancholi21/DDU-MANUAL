28deb5bdbfaa2ac0c01b105b743c9946d3670145
MANTRA PANCHOLI <mantra.b.pancholi@gmail.com>
2026-09-17 09:21:46 +0530

    Enhance max/min finding with index tracking and swapping
    Added index tracking for max and min values and updated the swapping logic to swap the elements in the array instead of just the values.

--- EXPERIMENT 5/Program4.c
+++ EXPERIMENT 5/Program4.c
@@ -1,12 +1,13 @@
 //EXPERIMENT 5
 //PROGRAM 4
-//FINDING MAXIMUM & MINIMUM VALUE WITHOUT SORTING VARIABLE
+// FINDING MAXIMUM & MINIMUM VALUE WITHOUT SORTING ARRAY
 //ALSO SWAPPING PLACE OF MAX & MIN
 #include <stdio.h>
 
 int main() 
 {
     int n,i,t;//var i is to control loop, t=temporary var,n=no. of element in array
+    int maxindex=0,minindex=0;//value of index(i) when a[i] max or min
     printf("ENTER NUMBER OF ELEMENT IN ARRAY :");
     scanf("%d",&n);
     //defining array a[n] after getting value of n
@@ -25,18 +26,29 @@ int main()
     for ( i = 0; i < n; i++)
     {
         if(a[i]>max)
-            max=a[i];//swapping max with a[i] if it is greater than max
+        {
+            max=a[i]; // updating max if a[i] is greater
+            maxindex=i;
+        }
         if(a[i]<min)
-            min=a[i];//swapping min with a[i] if it smaller than min
+        {
+            min=a[i]; // updating min if a[i] is smaller
+            minindex=i;
+        }
     }
     printf("\nMaximum value of array  = %d\n", max);//maximum of array
     printf("Minimum value of array = %d\n", min);  //minimum of array
     //SWAPPING MAX & MIN
-    t=max;
-    max=min;
-    min=t;
-    printf("\nSWAPPING MAX & MIN\n");
-    printf("\nSWAPED Maximum value of array = %d\n", max);//swaped max of array
-    printf("SWAPED Minimum value of array = %d\n", min);  //swaped min of array
+    t=a[minindex];
+    a[minindex]=a[maxindex];
+    a[maxindex]=t;
+
+    printf("\nARRAY AFTER SWAPPING MAX & MIN\n");
+    for(i=0;i<n;i++)            // PRINTING SWAPPED ARRAY
+    {
+        printf("%d",a[i]);
+        if(i<n-1)
+            printf(",");
+    }
     return 0;
 }
