1c61c4e6cbfc688e26d0e8ee8e262e65f243de66
nisargptl0410-ops <nisargptl04.10@gmail.com>
2026-09-20 23:43:16 +0530

    Update FtoC.c

--- EXPERIMENT 2/FtoC.c
+++ EXPERIMENT 2/FtoC.c
@@ -5,11 +5,19 @@
 #include<stdio.h>
 int main()
 {
+  //Declare variables to store Celsuis and Fahrenheit
   float C,F;
+  
+  //Ask user to enter the value
   printf("Enter the value of F: ");
+  
+  //Take Fahrenheit value as input from the user
   scanf("%f",&F);
 
+  //Convert Fahrenheit temp. to Celsuis
   C= (F-32)/1.8;
+
+  //Display the temp. in Celsuis
   printf("C=%f",C);
 
   return 0;
